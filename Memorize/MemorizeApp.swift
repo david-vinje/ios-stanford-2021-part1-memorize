a4bf32bcf71deaaeb9e48e274a53224d5b165e6d
David Nikolaj Vinje <91406860+david-vinje@users.noreply.github.com>
2021-11-12 11:24:46 +0100

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by David Nikolaj Vinje on 12/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
