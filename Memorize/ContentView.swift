167011923e1db3cc408892be93870f270ad0b3af
David Nikolaj Vinje <91406860+david-vinje@users.noreply.github.com>
2021-11-12 11:59:16 +0100

    Create HStack

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,7 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        CardView(emoji: "😇")
+        HStack {
+            CardView(emoji: "😇")
+            CardView(emoji: "😇")
+            CardView(emoji: "😇")
+        }
     }
 }
 
